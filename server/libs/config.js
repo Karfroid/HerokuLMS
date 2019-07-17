14d2fa11904998e4987869a83119cc4c818fe021
Karthik <prasanna.nagarajaiah@outlook.com>
2019-07-17 14:46:59 +0000

    only client files

--- server/libs/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secret: "B33ED1537DA7AE46C25C4A7CEF49D",
-  issuer: "TRIAD",
-  subject: "sibrahim@triadretail.com",
-  audience: "triadlms.com",
-  expires: "12h",
-  algorithm: "RS256"
-};
