3bc42289c5f6cb3ba191f32fb58b241db44e822e
Karthik <prasanna.nagarajaiah@outlook.com>
2019-07-17 18:18:33 +0000

    updated package.jason and server.js files

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require("express");
+const path = require("path");
 var cors = require("cors");
 var bodyParser = require("body-parser");
 var app = express();
 var port = process.env.PORT || 5000;
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
